4f21492ec8d7f7a52b2254070ae62f7a65bc4fbc
nicolimika <107408822+nicolimika@users.noreply.github.com>
2024-04-02 22:46:42 -0300

    Aula 5 concluida 02/04

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll(".aba-conteudo");
 
 for (let i=0; i < botoes.length; i++){
     botoes[i].onclick = function() {
 
         for (let j=0; j <botoes.length; j++){
             botoes[j].classList.remove("ativo");
+            texto[j].classList.remove("ativo");
         }
+        botoes[i].classList.add("ativo");
         botoes[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-05-10T23:59:59");
+let tempoAtual = new Date ();
+
+const contadores[0].textContent = tempoObjetivo1 - tempoAtual;
