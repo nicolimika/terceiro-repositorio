e951668db31bd47aac909ebd430db2438480c433
nicolimika <107408822+nicolimika@users.noreply.github.com>
2024-04-09 22:50:50 -0300

    Aula 6 finalizada, 09/04

--- main.js
+++ main.js
@@ -13,8 +13,25 @@ for (let i=0; i < botoes.length; i++){
     }
 }
 
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-10T23:59:59");
-let tempoAtual = new Date ();
 
-const contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+const contadores[0].textContent = calculaTempo(tempoObjetivo1) ;
+
+function calculaTempo(tempoObjetivo1){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Match.floor (segundos/60);
+    let horas = Match.floor (minutos/60);
+    let dias = Match.floor (horas/24);
+
+    segundos %=60;
+    minutos %=60;
+    horas %=24;
+
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+}
