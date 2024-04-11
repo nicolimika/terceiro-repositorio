bab58fc1c500cbfc3c1569029eed4de7f6434298
nicolimika <107408822+nicolimika@users.noreply.github.com>
2024-04-11 20:23:28 -0300

    aula 6 - finalizada - 11/04

--- main.js
+++ main.js
@@ -14,11 +14,14 @@ for (let i=0; i < botoes.length; i++){
 }
 
 
+
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo1 = new Date("2024-04-30T23:59:59");
+const tempoObjetivo2 = new Date("2024-05-10T23:59:59");
+const tempoObjetivo3 = new Date("2024-06-15T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-12T23:59:59");
 
-
-const contadores[0].textContent = calculaTempo(tempoObjetivo1) ;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date ();
